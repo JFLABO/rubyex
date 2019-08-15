ff2d28ea0b8b32a95c284f27a8a1c9f24caaddbb
JFLABO[JP] <vortex003@msn.com>
2019-08-16 05:44:47 +0900

    Update detect.rb

--- detect.rb
+++ detect.rb
@@ -3,5 +3,14 @@
 require File.dirname(__FILE__) + "/MyRegex"
 #調査
 o = MyRegex.new
-o.name = "taro"
+o.name = "みどりむし"
+
+p = MyRegex.new
+p.name = "みじんこ"
+
+q = MyRegex.new
+q.name = "いもむし"
+
 p o.name
+p p.name
+p q.name
