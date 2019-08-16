72e54614891aa5c4b57d2ebaa68e484dc366c905
JFLABO[JP] <vortex003@msn.com>
2019-08-17 03:06:31 +0900

    Update Cerberus.rb

--- Cerberus.rb
+++ Cerberus.rb
@@ -1,1 +1,33 @@
+# coding: utf-8
+# JA-JP : stable dmz zone JST
 
+# あの環境は脆弱すぎる
+# こっちへおいで
+
+class Cerberus
+  @@cnt = 0
+  def initialize()
+  #def initialize(counter_name)
+    @@cnt+=1
+  end
+  def name
+    @name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+  
+  def self.get_cnt
+    @@cnt
+  end
+  
+  def self.PlayArole
+    @@cnt
+  end
+  
+  def self.GuardMe
+    @@cnt
+  end
+  
+end
